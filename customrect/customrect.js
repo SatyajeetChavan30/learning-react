f51675df21b37925380a87e1a8ea646d4ac62d51
Satyajeet Chavan <satyajeetchavan30@gmail.com>
2025-07-04 19:07:19 +0530

    make a custom react library

---
+++ customrect/customrect.js
@@ -0,0 +1,19 @@
+function customreact(reactElement, mainContainer) {
+  let DOMelement = document.createElement(reactElement.type);
+  DOMelement.innerHTML = reactElement.children;
+  DOMelement.setAttribute("href", reactElement.props.href);
+  DOMelement.setAttribute("target", reactElement.props.target);
+  mainContainer.appendChild(DOMelement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "www.google.com",
+    target: "_blank",
+  },
+  children: "Click On Me To Gooo",
+};
+
+const mainContainer = document.querySelector(".root");
+customreact(reactElement, mainContainer);
