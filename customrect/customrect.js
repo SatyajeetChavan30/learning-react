2b9c760352a19d91945c42d94450d4b7f8c0d910
Satyajeet Chavan <satyajeetchavan30@gmail.com>
2025-07-04 19:11:55 +0530

    optimize custom react library

--- customrect/customrect.js
+++ customrect/customrect.js
@@ -1,8 +1,14 @@
 function customreact(reactElement, mainContainer) {
+  //   let DOMelement = document.createElement(reactElement.type);
+  //   DOMelement.innerHTML = reactElement.children;
+  //   DOMelement.setAttribute("href", reactElement.props.href);
+  //   DOMelement.setAttribute("target", reactElement.props.target);
+  //   mainContainer.appendChild(DOMelement);
   let DOMelement = document.createElement(reactElement.type);
   DOMelement.innerHTML = reactElement.children;
-  DOMelement.setAttribute("href", reactElement.props.href);
-  DOMelement.setAttribute("target", reactElement.props.target);
+  for (const key in reactElement.props) {
+    DOMelement.setAttribute(key, reactElement.props[key]);
+  }
   mainContainer.appendChild(DOMelement);
 }
 
